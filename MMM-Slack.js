ccaf9d3b364a3f4b3e07924f7b144457128911e8
Jupter1 <39801700+Jupter1@users.noreply.github.com>
2020-04-13 17:19:08 +0200

    Update MMM-Slack.js

--- MMM-Slack.js
+++ MMM-Slack.js
@@ -4,7 +4,7 @@ Module.register('MMM-Slack',{
 	showTime: true,
 	showSeconds: false,
 	maxUsers: 3,
-	refreshTime: 60000,
+	interval: 60000,
 	displayTime: 600,
 	urgentRefresh: false,
 	animationSpeed: 1000
@@ -26,7 +26,7 @@ Module.register('MMM-Slack',{
         	var self = this;
         	setInterval(function() {
         		self.updateDom(this.config.animationSpeed);
-        	}, self.config.refreshTime);
+        	}, self.config.interval);
 	},
 
 	openSlackConnection: function() {
